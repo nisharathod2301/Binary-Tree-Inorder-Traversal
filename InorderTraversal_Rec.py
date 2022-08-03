a4a803872b2bdf361d7de8d812ab1eda2d2e1d92
nisharathod2301 <nisharathod232001.com>
2022-08-03 16:13:50 +0530

    Initial

---
+++ InorderTraversal_Rec.py
@@ -0,0 +1,20 @@
+#This is Iterative Solution, using stack 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        traverse=[]
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            traverse.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return traverse
